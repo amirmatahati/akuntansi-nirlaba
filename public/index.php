ebf1be96a02e50bdc2d8b49c40ea08470fff9d8b
amirmatahati <amirrudi2610@bsi.ac.id>
2018-10-06 12:11:33 +0700

    remove public url

--- public/index.php
+++ public/index.php
@@ -1,5 +1,11 @@
 <?php
 
+error_reporting(0);
+error_reporting(E_ERROR | E_WARNING | E_PARSE);
+error_reporting(E_ALL);
+ini_set("error_reporting", E_ALL);
+error_reporting(E_ALL & ~E_NOTICE);
+
 /**
  * Laravel - A PHP Framework For Web Artisans
  *
